6d35391c887ea53e62c81ed649fb30128722d32d
Shahoriar <shahoriar.fahim@gmail.com>
2023-05-08 13:24:17 +0600

    feat: Add logout button

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -1,24 +1,34 @@
 <!-- Navbar Starts -->
 <nav class="navbar navbar-expand-custom navbar-mainbg">
-  <a class="navbar-brand navbar-logo" href="#">TodoList</a>
-  <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <i class="fas fa-bars text-white"></i>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <div class="hori-selector">
-        <div class="left"></div>
-        <div class="right"></div>
-      </div>
-      
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('all_lists') }}"><i class="far fa-address-book"></i>All Lists</a>
-      </li>
-      
-    </ul>
-  </div>
+    <a class="navbar-brand navbar-logo" href="#">TodoList</a>
+    <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <i class="fas fa-bars text-white"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <div class="hori-selector">
+                <div class="left"></div>
+                <div class="right"></div>
+            </div>
+
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('all_lists') }}"><i class="far fa-address-book"></i>All Lists</a>
+            </li>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+            <li class="nav-item ">
+                <a class="nav-link" href="#"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="far fa-address-book"></i>
+                    Logout
+                </a>
+            </li>
+
+        </ul>
+    </div>
 </nav>
 
 <!-- Navbar Ends -->
-
-
